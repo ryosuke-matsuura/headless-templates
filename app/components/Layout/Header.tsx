71cf8517eba9b0c80d2cc517475f997ee6e2e30b
Munehiro Yamazaki <yama.munehiro@gmail.com>
2025-04-04 22:42:47 +0900

    コミットメッセージ

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { CartBag } from '@app/components/CartBag/CartBag';
-import logo from '../../../src/assets/dotpb-logo2.png';
 import { NavBar } from './NavBar/NavBar';
 import { Login } from '@app/components/Login/Login';
 import testIds from '@app/utils/test-ids';
@@ -15,7 +14,7 @@ const Header = () => (
         <h2 className="flex-1">
           <Image
             className="navpic items-center ml-0 h-14"
-            src={logo}
+            src="https://static.wixstatic.com/media/a20eb7_2346bbee2fd142a388fcf01ae97325d6~mv2.png/v1/fill/w_95,h_65,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/dotpb_logo2.png"
             alt="Placeholder Image"
             width={56}
             height={56}
