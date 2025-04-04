364cfb6513ed8ed52b241c4f5b98c47682f8241e
Munehiro Yamazaki <yama.munehiro@gmail.com>
2025-04-04 22:54:18 +0900

    コミットメッセージ

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { CartBag } from '@app/components/CartBag/CartBag';
 import { NavBar } from './NavBar/NavBar';
 import { Login } from '@app/components/Login/Login';
@@ -12,12 +11,11 @@ const Header = () => (
     >
       <div className="flex px-6 sm:px-14 h-header items-center gap-4 sm:gap-8">
         <h2 className="flex-1">
-          <Image
-            className="navpic items-center ml-0 h-14"
-            src="https://static.wixstatic.com/media/a20eb7_2346bbee2fd142a388fcf01ae97325d6~mv2.png/v1/fill/w_95,h_65,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/dotpb_logo2.png"
-            alt="Placeholder Image"
-            width={56}
-            height={56}
+          <div
+            className="navpic items-center ml-0 h-14 w-14 bg-no-repeat bg-contain bg-center"
+            style={{
+              backgroundImage: 'url(https://static.wixstatic.com/media/a20eb7_2346bbee2fd142a388fcf01ae97325d6~mv2.png/v1/fill/w_95,h_65,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/dotpb_logo2.png)'
+            }}
           />
         </h2>
         <div>
