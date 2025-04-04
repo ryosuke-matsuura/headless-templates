efce25b8476871f0be9b5da72a338df61e093081
Munehiro Yamazaki <yama.munehiro@gmail.com>
2025-04-04 14:35:54 +0900

    コミットメッセージ

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => (
     data-testid={testIds.LAYOUT.FOOTER}
   >
     <IndexSectionCustomComponents1 />
-    <div className="mx-auto flex flex-col-reverse items-center sm:flex-row gap-1 sm:gap-16 pt-3 sm:pt-2 pb-2 sm:pb-4 px-6 sm:px-14 text-12 sm:text-xs">
+    <div
+      className="mx-auto flex flex-col-reverse items-center sm:flex-row gap-1 sm:gap-16 pt-3 sm:pt-2 pb-2 sm:pb-4 px-6 sm:px-14 text-12 sm:text-xs"
+    >
       <p className="mb-2 flex-1 text-center">© 2024 Pure Biokeyz Japan Inc.</p>
     </div>
   </footer>
